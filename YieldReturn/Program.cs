ab047bcb9f5528dbb58a82e0271a2b297c7bee66
AngasL <yueguang0514@126.com>
2016-07-29 09:20:44 +0800

    Add yield return project.

---
+++ YieldReturn/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YieldReturn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var people = new People
+            {
+                Pencil = new List<string> { "pencile1", "pencile2"},
+                BallPointPen = new List<string> { "ballpointpen1","ballpointpen2"}
+            };
+
+            var pens = GetAllPens(people);
+
+            foreach (var pen in pens)
+            {
+                Console.WriteLine(pen);
+            }
+        }
+
+        private static IEnumerable<string> GetAllPens(People people)
+        {
+            foreach (var pencil in people.Pencil)
+            {
+                yield return pencil;
+            }
+
+            foreach (var ballpointpen in people.BallPointPen)
+            {
+                yield return ballpointpen;
+            }
+        }
+    }
+
+    public class People
+    {
+        public IEnumerable<string> Pencil { get; set; }
+        public IEnumerable<string> BallPointPen { get; set; }
+    }
+}
