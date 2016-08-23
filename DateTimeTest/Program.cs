8e6e67c13c9ebaabc0975de3d7a08a0fe1ddf2ef
AngasL <yueguang0514@126.com>
2016-08-23 15:04:04 +0800

    Sync code.

---
+++ DateTimeTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dateTimeNow = DateTime.UtcNow;
+
+            var addedDateTime = dateTimeNow.AddSeconds(1);
+
+            Console.WriteLine(dateTimeNow.Ticks);
+            Console.WriteLine(addedDateTime.Ticks);
+        }
+    }
+}
--- TestString/Program.cs
+++ TestString/Program.cs
@@ -10,6 +10,7 @@ namespace TestString
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(7.5M);
         }
     }
 
