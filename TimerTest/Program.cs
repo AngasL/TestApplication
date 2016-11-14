ad26c9fb6f29595ea5ba66a6454852500743ff08
AngasL <yueguang0514@126.com>
2016-11-14 09:50:11 +0800

    Add timer demo

---
+++ TimerTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Timers;
+
+namespace TimerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var timer = new Timer(0);
+            timer.Elapsed += (e, arg) => { DispayMethed(); };
+
+            var startTime = DateTime.Now;
+
+            while (DateTime.Now.Millisecond - startTime.Millisecond < 5)
+            {
+                
+            }
+
+            timer.Stop();
+        }
+
+        static void DispayMethed()
+        {
+            Console.WriteLine(DateTime.Now);
+        }
+    }
+}
